749e53963942ddc6afcaa1eca6ab8fc402da153b
Benjamin Höglinger <nefarius@dhmx.at>
2017-07-01 12:59:41 +0200

    Introduced settings

--- Symphonia/MainWindow.xaml.cs
+++ Symphonia/MainWindow.xaml.cs
@@ -1,13 +1,32 @@
-﻿namespace Symphonia
+﻿using System.IO;
+using System.Reflection;
+using Antlr4.StringTemplate;
+using Symphonia.Properties;
+
+namespace Symphonia
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Symphonia.DLNA.SOAP.Synology.ContentDirectory.Browse.txt";
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream))
+            {
+                var result = reader.ReadToEnd();
+
+                var st = new Template(result, Settings.Default.TemplateStartCharacter, Settings.Default.TemplateStopCharacter);
+                st.Add("Filter", "*");
+
+                var t = st.Render();
+            }
         }
     }
 }
